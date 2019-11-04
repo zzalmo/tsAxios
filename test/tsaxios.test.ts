c0083daa5b60826758d0bb37b4b6e27864b00a97
zhouyi14 <zhouyi14@58.com>
2019-11-04 15:59:41 +0800

    fix(tsaxios.test.ts): test the cz
    test

--- test/tsaxios.test.ts
+++ test/tsaxios.test.ts
@@ -3,7 +3,7 @@ import DummyClass from '../src/tsaxios'
 /**
  * Dummy test
  */
-describe('Dummy test', () => {
+describe('Dummy test1', () => {
   it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
