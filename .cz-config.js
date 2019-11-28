8838069c616aa12f9a7a2dea3a24f1a05c3aefc8
zhouyi14 <zhouyi14@58.com>
2019-11-28 17:32:33 +0800

    [feat](accounts): dsfa

--- .cz-config.js
+++ .cz-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat:     A new feature' },
-    { value: 'fix', name: 'fix:      A bug fix' },
-    { value: 'docs', name: 'docs:     Documentation only changes' },
+    { value: '[feat]', name: 'feat:     A new feature' },
+    { value: '[fix]', name: 'fix:      A bug fix' },
+    { value: '[docs]', name: 'docs:     Documentation only changes' },
     {
       value: 'style',
       name:
